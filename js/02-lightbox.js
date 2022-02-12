972b9f6f3d0ffda5b390616548b8ec86f9d16a43
ElenaLukashevych <elukashevych@gmail.com>
2022-02-12 18:35:19 +0200

    task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+//  console.log(galleryEl);
+
+function createGalleryMarkup(items) {
+    return items.map(({ preview, original, description }) =>
+        `<li> <a class="gallery__item" href=${original}>
+  <img class="gallery__image" src=${preview} alt=${description} />
+</a></li>`
+    ).join('');
+
+};
+
+
+const listItemsMarkup = createGalleryMarkup(galleryItems);
+
+galleryEl.insertAdjacentHTML('beforeend', listItemsMarkup);
+
+
+
+const lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250, captionsData: 'alt' });
+
+  
+
+    
+
